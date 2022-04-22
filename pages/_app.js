5a00d3841332476c1fd4244f2146fa12a7a8e729
ArthemisCOF <Sorawit.Ily@hotmail.com>
2022-04-23 03:36:49 +0700

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../src/config/firebase.config'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ src/config/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIP-3Nto6PFMueqRmHtNNDMTXL2_5MqsA",
+    authDomain: "auth-next-6514d.firebaseapp.com",
+    projectId: "auth-next-6514d",
+    storageBucket: "auth-next-6514d.appspot.com",
+    messagingSenderId: "1028284565083",
+    appId: "1:1028284565083:web:597a8cc5412b426937217e"
+  };
+
+const app = initializeApp(firebaseConfig);
