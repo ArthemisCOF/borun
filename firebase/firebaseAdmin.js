961667717d3102f954a41486d87ab9c0b77fc83a
ArthemisCOF <Sorawit.Ily@hotmail.com>
2022-04-24 01:18:10 +0700

    firebase

---
+++ firebase/firebaseAdmin.js
@@ -0,0 +1,14 @@
+const admin = require('firebase')
+const serviceAccount = require('./secrets.json')
+
+export const verifyIdToken = (token) => {
+    if (!admin.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "auth-next-5a010.firebaseapp.com",
+        })
+    }
+    return admin.auth().verifyIdToken(token).catch((error) => {
+        throw error
+    })
+}
